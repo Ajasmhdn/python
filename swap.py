3d5aa21dfb0642cfce070bb5b95ed6ae4fb5ffcc
Ajasmhdn <ajasaju404@gmail.com>
2023-12-19 11:00:36 +0530

    Second day

---
+++ swap.py
@@ -0,0 +1,7 @@
+num1=input("Enter the first number :")
+num2=input("Enter the Second number :")
+temp=num1
+num1=num2
+num2=temp
+print("the First no now is " + num1)
+print("the Second no now is " + num2)
---
+++ var.py
@@ -0,0 +1,4 @@
+a=input("enter the name of the movie:")
+b=len(a)
+print(b)
+print("The name of the movie is "+ a)
